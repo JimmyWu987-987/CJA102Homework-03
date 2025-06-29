012e192f930bf5273b96753523d4371480c643d0
JimmyWu <workjimmywu0820@gmail.com>
2025-06-29 18:13:19 +0800

    Update hw3_Q1<ver.1.0.2>

--- src/hw3/hw3_Q1.java
+++ src/hw3/hw3_Q1.java
@@ -1,47 +1,53 @@
 package hw3;
 
 import java.util.Arrays;
-
-//請設計一隻程式，使用者輸入三個數字後，輸出結果會為正三角形、等腰
-//三角形、其它三角形或不是三角形，如圖示結果：
+/*
+請設計一隻程式，使用者輸入三個數字後，輸出結果會為正三角形、等腰
+三角形、其它三角形或不是三角形，如圖示結果：
+*/
 
 public class hw3_Q1 {
 	public static void main(String[] args) {
 
 		int[] input = new int[3];
-//		int output = 0;
 		ScannerObject SO = new ScannerObject();
 
 		System.out.println("請輸入3個數字,輸入完後請按Enter：");
 
 		for (int i = 0; i < input.length; i++) {
 			input[i] = SO.KeyIn();
-//			output += input[i];
 		}
-		//先做由小至大的排序,這樣保證input[2]就是最長邊
-		Arrays.sort(input);
 
-		if ((input[0] + input[1]) > input[2]) { // 最小兩邊和 > 最長邊 為三角形
-			if (input[0] == input[1]) {
+		Arrays.sort(input); // 先做由小至大的排序,這樣保證input[2]就是最長邊
+		triangleJudge(input[0], input[1], input[2]); // 判斷三角形的方法
+
+	}
+
+	/*
+	 * a, b 為短邊 c 為最長邊。最小兩邊和 > 最長邊為三角形。此方法為判斷三角形的方法
+	 */
+	public static void triangleJudge(int a, int b, int c) {
+
+		if ((a + b) > c) {
+			if (a == b) {
 				System.out.println("等腰三角形");
-			} else if (input[0] == input[2]) {
+			} else if (a == c) {
 				System.out.println("正三角形");
-			} else if (pythThm(input[0], input[1], input[2])) {
+			} else if (pythThm(a, b, c)) {
 				System.out.println("直角三角形");
 			} else {
 				System.out.println("其他三角形");
 			}
-
 		} else {
 			System.out.println("不是三角形");
 		}
-
 	}
 
-	public static boolean pythThm(int a, int b, int c) { 
-		// c為最長邊
-		// 利用畢氏定理判斷是否為直角三角形
-		
+	/*
+	 * 畢氏定理的方法，判斷是否為直角三角形，a, b 為短邊 c 為最長邊。
+	 */
+	public static boolean pythThm(int a, int b, int c) {
+
 		if ((c * c) == ((a * a) + (b * b))) {
 			return true;
 		} else {
