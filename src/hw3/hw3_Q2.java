3b7e2c1188ea060f858f7e8a57dfcbedd58aa55b
JimmyWu <workjimmywu0820@gmail.com>
2025-06-29 09:03:02 +0800

    hw3_Q2 frist commit

--- src/hw3/hw3_Q2.java
+++ src/hw3/hw3_Q2.java
@@ -4,5 +4,16 @@ package hw3;
 //對則顯示正確訊息，如圖示結果：
 
 public class hw3_Q2 {
+	public static void main(String[] args) {
 
+		int input, output = 0;
+		ScannerObject SO = new ScannerObject();
+		output = (int) ((Math.random() * 9) + 1);
+
+		System.out.println("數字猜猜看！請輸入0~9任意數字！");
+		do {
+			input = SO.KeyIn();
+		}while(true);
+		
+	}
 }
