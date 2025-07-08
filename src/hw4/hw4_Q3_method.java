6f42be1234a216067f79f96ae6b567bf459c2bfa
JimmyWu <workjimmywu0820@gmail.com>
2025-07-08 08:52:50 +0800

    Fix litte mastake

--- src/hw4/hw4_Q3_method.java
+++ src/hw4/hw4_Q3_method.java
@@ -1,17 +1,19 @@
 package hw4;
 
 /*
+ * 此類別是給『hw4_Q3.java』使用
  * 有個字串陣列如下 (八大行星)：
  * {“mercury”, “venus”, “earth”, “mars”, “jupiter”, “saturn”, “uranus”, “neptune”}
  * 請用程式計算出這陣列裡面共有多少個母音(a, e, i, o, u)
  * (提示：字元比對，String方法)
 */
-public class hw4_Q3 {
-
-	private final String[] EIGHT_PLANETS = {"mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"};
+public class hw4_Q3_method {
 	
-	public static void main(String[] args) {
-
-	}
+	private int NumberOfVowels;
+	
+	
+	// 利用switch找出母音的方法
+	
+	// 印出共有多少個母音
 
 }
