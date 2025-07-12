e490638740d374cb159db6517ac9d4bb750008c9
JimmyWu <workjimmywu0820@gmail.com>
2025-07-12 19:24:28 +0800

    Finsh the code of the hw5_Q1 and rename it name of hw5_Q1<ver1.0.0>

--- src/hw5/hw5_Q1.java
+++ src/hw5/hw5_Q1.java
@@ -1,9 +1,58 @@
 package hw5;
 
-
 //請設計一個方法為starSquare(int width, int height)，當使用者鍵盤輸入寬與高時，即會印出對應的*長方形，如
 //圖：
 
+import hw3.ScannerObject;
+
 public class hw5_Q1 {
 
+	static {
+		System.out.println("請輸入寬與高：");
+	}
+
+	public static void main(String[] args) {
+		starSquare ss = new starSquare();
+	}
+
+}
+
+class starSquare {
+	private int width;
+	private int height;
+	ScannerObject sc = new ScannerObject();
+	
+	public starSquare(){
+		
+		setWidth();
+		setHeight();
+		getStarSquare(getWidth(),getHeight());
+			
+			
+	}
+
+	public void getStarSquare(int width, int height) {
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public void setWidth() {
+		this.width = sc.setKeyIn();
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public void setHeight() {
+		this.height = sc.setKeyIn();
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
 }
